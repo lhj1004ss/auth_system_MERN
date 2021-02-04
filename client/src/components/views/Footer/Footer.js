92c01b1133332c266eb5fda998f93358b16c3c9d
hyojelee <hyojelee1004ss@gmail.com>
2021-02-04 14:50:52 -0500

    implemented login, logout and auth

---
+++ client/src/components/views/Footer/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div style={{
+            height: '80px', display: 'flex',
+            flexDirection: 'column', alignItems: 'center',
+            justifyContent: 'center', fontSize:'1rem'
+        }}>
+           <p>Hyoje Lee</p>
+           <p>Computer Engineering Technology</p>
+           <p>Humber College</p>
+           <p>hyojelee1004ss@gmail.com</p>
+           <p>647-781-3802</p>
+        </div>
+    )
+}
+
+export default Footer
---
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function LandingPage() {
+    return (
+        <>
+            <div className="app">
+              landingpage
+            </div>
+        </>
+    )
+}
+
+export default LandingPage
